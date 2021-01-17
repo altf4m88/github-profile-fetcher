ccfe3459e5be150312424ff33dcf0e555f5bda9e
altf4m88 <fadhilmauladhani@gmail.com>
2021-01-17 19:15:42 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Nav from './Components/Nav/Nav';
+import Main from './Components/GitHub/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+        <Nav />
+        <Main />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
