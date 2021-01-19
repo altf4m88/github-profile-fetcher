17c47aa277ec495e0f2cbfae15856168d77f2bd4
altf4m88 <fadhilmauladhani@gmail.com>
2021-01-19 19:31:44 +0700

    create new component repositories.jsx

---
+++ src/Components/GitHub/Repositories.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Repositories extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { };
+    }
+    render(){
+        return(
+
+        );
+    }
+}
+
+export default Repositories;
