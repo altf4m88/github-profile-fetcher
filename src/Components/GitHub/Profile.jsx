34e74a2dbd990afd5c9414734ea2ecfb58141e97
altf4m88 <fadhilmauladhani@gmail.com>
2021-01-19 19:29:14 +0700

    set api request for fetching user data

--- src/Components/GitHub/Profile.jsx
+++ src/Components/GitHub/Profile.jsx
@@ -3,24 +3,67 @@ import React, { Component } from 'react'
 class Profile extends Component{
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            user : []
+        };
+
+        const fetchProfile = async (user) => {
+            const apiRequest = await fetch(`https://api.github.com/users/${user}`);
+            const dataResult = await apiRequest.json();
+            return dataResult;
+        }
+        //init fetch method
+        fetchProfile(props.match.params.login).then((res) => {
+            if(res){
+                this.setState({user: res});
+            }
+        });
     }
-    render(){
-        return (
-            <React.Fragment>
-            <center>
+
+    Profile(){
+        if(this.state.user.login === 0){
+            return(
+            <p>No user found with username : {this.props.match.params.login}</p>
+            );
+        } else{
+            return(
+                <center>
                 <section className="Specific">
                 <div className="container">
                 <div className="main" id="main">
                 <div className="row">
                 <div className="col-lg-12 col-md-12">
                     <div className="SUsersData">
-                    <i className="fas fa-heart  NotFave"></i><h4>Name :<i class="bl">fahem ahemd</i>   </h4>
-                    <img src="https://avatars3.githubusercontent.com/u/10535083?v=4" alt="" />
-                    <h4>followers :<i className="bl"> 3 </i>  </h4> 
-                    <h4>location :<i className="bl"> dhaka,bangladesh </i> </h4>
-                    <div>
-                    <div>
+                    <i className="fas fa-heart  NotFave"></i>
+                    <h4>Name :
+                    <i className="bl">{this.state.user.name}</i>   
+                    </h4>
+                    <img src={this.state.user.avatar_url} alt="" />
+            <h4>Followers :<i className="bl">{this.state.user.followers}</i>  </h4> 
+            <h4>Location :<i className="bl">{this.state.user.location}</i> </h4>
+
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+                </section>
+                </center>
+
+
+            )
+        }
+
+
+
+    }
+
+
+    render(){
+        return (
+            <React.Fragment>
+                {this.Profile()}
+            
                         <h3>here Last 5 repo</h3>
                         <div className="lastfiveRepo">
                         <div>
@@ -45,16 +88,7 @@ class Profile extends Component{
                             <a href="https://github.com/f4h3m/dope"> dope </a>
                         </div>
                         </div>
-                        </div>
-
-                    </div>
-                    </div>
-                </div>
-                </div>
-                </div>
-                </div>
-                </section>
-            </center>
+                        
             </React.Fragment>
         );
     }
