eb109c0949701d4f07c785f3c327e90772aa54a4
altf4m88 <fadhilmauladhani@gmail.com>
2021-03-10 09:40:29 +0700

    create register and login route

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const router = express.Router();
+const route = express.Router();
 
 const connection = mysql.createConnection({
     host:'127.0.0.1',
@@ -28,4 +28,45 @@ const selectOrCreateTable = () => {
 
 selectOrCreateTable();
 
-module.exports = router;
+route.post('/register', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    connection.query(`SELECT * FROM users WHERE email = '${email}'`, (err, result) => {
+        if(err){
+            res.send({err : err});
+        }
+        if(result.length === 0){
+            let query = `INSERT INTO users(name, email, password) VALUES('${name}', '${email}', '${password}')`;
+            connection.query(query, (err, result) => {
+                if(err) throw err;
+                res.status(200).send({result});
+                console.log(result);
+            });
+        } else {
+            return res.status(201).send({message : 'Email has already taken'});
+        }
+    });
+})
+
+const jwtPrivateSecret = "";
+
+route.post('/login', async(req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    connection.query(`SELECT * FROM users WHERE email='${email}' AND password= '${password}'`, async (err, result) => {
+        if(result.length !== 0){
+            jwt.sign({UserEmail: email}, jwtPrivateSecret, (err, token) => {
+                res.status(200).send({token : token});
+            });
+        }
+        if(result.length === 0){
+            res.status(400).send({message:'Error not found'});
+        }
+    });
+})
+
+
+module.exports = route;
